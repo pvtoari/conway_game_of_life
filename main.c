9b55ecea3d91b35e75e2d90d601666a99dd7f526
shardassaut <sharhabia@gmail.com>
2025-05-24 20:32:53 +0200

    Add base file

---
+++ main.c
@@ -0,0 +1,28 @@
+#define COL 3
+#define ROW 3
+
+//TO DO:
+// - Read a .txt with the grid to get the base plan
+// - Cell life rules
+//      - /!\ overflow for checking around
+
+int main(){
+    int neighbours;
+    int grid[COL][ROW] = {{0,0,0},{1,1,1},{0,0,0}};
+    int buffer[COL][ROW] = {0};
+
+    for(int i=0;i < COL; i++){
+        for(int j=0;j<ROW;i++){
+            neighbours = 0;
+
+        }
+    }
+    
+
+
+
+
+
+
+    return 0;
+}
