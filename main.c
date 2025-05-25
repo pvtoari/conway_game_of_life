d4c5c8817dcc44cd769a96bc2f7ad8066d247886
shardassaut <sharhabia@gmail.com>
2025-05-25 09:23:14 +0200

    Add better check_neighbour

--- main.c
+++ main.c
@@ -8,47 +8,29 @@
 
 #define COL 3
 #define ROW 3
-#define GEN 5
+#define GEN 10
 
 void check_neihbours(int grid[COL][ROW],int i, int j,int *pt){
-        if( i - 1 >= 0){
-            if(grid[i-1][j] == 1) (*pt)++;
-        }
-        if( i + 1 < COL){
-            if(grid[i+1][j] == 1) (*pt)++;
-        }
+        
+        for(int ai=-1;ai<2;ai++){
+            if(i+ai <0 || i+ai >= ROW )continue;
+            for(int aj=-1;aj<2;aj++){
+                if(j+aj <0 || j+aj >= COL || (aj == 0 && ai == 0))continue;
+                if(grid[i+ai][j+aj] == 1 ) (*pt)++;
+            }
 
-
-        if( j - 1 >= 0){
-            if(grid[i][j-1] == 1) (*pt)++;
         }
-        if( j + 1 < ROW){
-            if(grid[i][j+1] == 1) (*pt)++;
-        }
-    if(i - 1 >= 0 ){
-        if( j - 1 >= 0){
-            if(grid[i- 1 ][j- 1 ] == 1) (*pt)++;
-        }
-        if( j + 1 < ROW){
-            if(grid[i- 1 ][j+ 1 ] == 1) (*pt)++;
-        }
-    }
-    if(i + 1 < COL){
-        if( j - 1 >= 0){
-            if(grid[i+1][j- 1 ] == 1) (*pt)++;
-        }
-        if( j + 1 < ROW){
-            if(grid[i+ 1 ][j+ 1 ] == 1) (*pt)++;
-        }
-    }
+        //printf("neighbours: %d for cell [%d,%d]\n",*pt,i,j);
 }
 
 int main(){
     int neighbours, n=0, u;
-    int grid[COL][ROW] = {{0,0,0},{1,0,1},{0,1, 0}};
+    int grid[COL][ROW] = {{1,1,0},{1,1,1},{0,0, 0}};
     int buffer[COL][ROW]={{0,0,0},{0,0,0},{0,0, 0}};
 
-    while(n<GEN){
+
+
+   while(n<GEN){
         for(int i=0;i < COL; i++){
             for(int j=0;j< ROW;j++){
                 neighbours = 0;
@@ -78,10 +60,10 @@ int main(){
             printf("\n");
         }
         if(u == 0){
-            printf("\nSimulación acabada a la gen: %d\n",n);
+            printf("\nSimulación acabada a la gen: %d\n",n+1);
             break;
         }
-        printf("\n");
+        printf("\nSimulación nº%d\n",n+1);
         n++;
     }
 
